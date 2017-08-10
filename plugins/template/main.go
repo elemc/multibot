34620b904e183b6904532c9a37dbf051b90c440e
elemc <me@elemc.name>
2017-08-10 15:02:58 +0300

    added template plugin source code

---
+++ plugins/template/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"multibot/context"
+
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+var ctx *context.MultiBotContext
+
+// InitPlugin initialize plugin if it needed
+func InitPlugin(mbc *context.MultiBotContext) error {
+	ctx = mbc
+	return nil
+}
+
+// GetName function returns plugin name
+func GetName() string {
+	return "template"
+}
+
+// GetDescription function returns plugin description
+func GetDescription() string {
+	return "This is template plugin for multibot"
+}
+
+// GetCommands return plugin commands for bot
+func GetCommands() []string {
+	return []string{}
+}
+
+// UpdateHandler function call for each update
+func UpdateHandler(update tgbotapi.Update) (err error) {
+	return nil
+}
+
+// RunCommand handler start if bot get one of commands
+func RunCommand(command string, update tgbotapi.Update) (err error) {
+	return
+}
